76c02aa56b4d0039dd8fa9aeb5c7b8d39076fae0
Thaisy Morais <thaisy-ms@outlook.com>
2022-04-07 23:24:27 -0300

    exercicios 2

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,5 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index += 1) {
+for (let index1 = 0; index1 < numbers; index1 += 1){
   console.log(numbers[index])
 }
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+console.log(soma);
