5a1a58459d3bd21751884bfa5881363f4f54a73c
Thaisy Morais <thaisy-ms@outlook.com>
2022-05-17 11:33:58 -0300

    dia 1 ok

---
+++ Fundamentos/Bloco-7/dia-1-ES6/script.js
@@ -0,0 +1,22 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    var ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope}. ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    var elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+    console.log(elseScope);
+  }
+  console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function ordenaNumeros() {
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort()
+}
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente`)
